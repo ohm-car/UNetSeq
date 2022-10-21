e27e4c96b37b5adbf48705f3f0b8289c7c4f83e5
omkar-umbc <omkark1@umbc.edu>
2022-10-21 16:03:30 -0400

    Complete toy model done

--- k_train.py
+++ k_train.py
@@ -2,8 +2,12 @@ import tensorflow as tf
 from utils.k_dataset import DatasetTrial
 from unet.unet_model_keras import UNet
 
-unet_model = UNet().create_model()
 dataset = DatasetTrial()
+data_tr = dataset.get_train_dataset()
+data_ts = dataset.get_test_dataset()
+info = dataset.get_info()
+
+unet_model = UNet().create_model()
 
 print(type(unet_model))
 # unet_model.build(input_shape = (128,128,3))
@@ -13,18 +17,25 @@ unet_model.compile(optimizer=tf.keras.optimizers.Adam(),
 
 print(unet_model.summary())
 
-# NUM_EPOCHS = 20
+BATCH_SIZE = 16
+BUFFER_SIZE = 1000
+train_batches = data_tr.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_batches = train_batches.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+validation_batches = data_ts.take(3000).batch(BATCH_SIZE)
+test_batches = data_ts.skip(3000).take(669).batch(BATCH_SIZE)
 
-# TRAIN_LENGTH = info.splits["train"].num_examples
-# STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+NUM_EPOCHS = 20
 
-# VAL_SUBSPLITS = 5
-# TEST_LENTH = info.splits["test"].num_examples
-# VALIDATION_STEPS = TEST_LENTH // BATCH_SIZE // VAL_SUBSPLITS
+TRAIN_LENGTH = info.splits["train"].num_examples
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
 
-# model_history = unet_model.fit(train_batches,
-#                               epochs=NUM_EPOCHS,
-#                               steps_per_epoch=STEPS_PER_EPOCH,
-#                               validation_steps=VALIDATION_STEPS,
-#                               validation_data=test_batches)
+VAL_SUBSPLITS = 5
+TEST_LENTH = info.splits["test"].num_examples
+VALIDATION_STEPS = TEST_LENTH // BATCH_SIZE // VAL_SUBSPLITS
+
+model_history = unet_model.fit(train_batches,
+                              epochs=NUM_EPOCHS,
+                              steps_per_epoch=STEPS_PER_EPOCH,
+                              validation_steps=VALIDATION_STEPS,
+                              validation_data=test_batches)
 
--- utils/k_dataset.py
+++ utils/k_dataset.py
@@ -15,13 +15,13 @@ class DatasetTrial(object):
 		# self.arg = arg
 		
 
-	def resize(input_image, input_mask):
+	def resize(self, input_image, input_mask):
 		input_image = tf.image.resize(input_image, (128, 128), method="nearest")
 		input_mask = tf.image.resize(input_mask, (128, 128), method="nearest")
 
 		return input_image, input_mask
 
-	def augment(input_image, input_mask):
+	def augment(self, input_image, input_mask):
 		if tf.random.uniform(()) > 0.5:
 			# Random flipping of the image and mask
 			input_image = tf.image.flip_left_right(input_image)
@@ -29,37 +29,40 @@ class DatasetTrial(object):
 
 		return input_image, input_mask
 
-	def normalize(input_image, input_mask):
+	def normalize(self, input_image, input_mask):
 		input_image = tf.cast(input_image, tf.float32) / 255.0
 		input_mask -= 1
 		return input_image, input_mask
 
-	def load_image_train(datapoint):
+	def load_image_train(self, datapoint):
 		input_image = datapoint["image"]
 		input_mask = datapoint["segmentation_mask"]
-		input_image, input_mask = resize(input_image, input_mask)
-		input_image, input_mask = augment(input_image, input_mask)
-		input_image, input_mask = normalize(input_image, input_mask)
+		input_image, input_mask = self.resize(input_image, input_mask)
+		input_image, input_mask = self.augment(input_image, input_mask)
+		input_image, input_mask = self.normalize(input_image, input_mask)
 
 		return input_image, input_mask
 
-	def load_image_test(datapoint):
+	def load_image_test(self, datapoint):
 		input_image = datapoint["image"]
 		input_mask = datapoint["segmentation_mask"]
-		input_image, input_mask = resize(input_image, input_mask)
-		input_image, input_mask = normalize(input_image, input_mask)
+		input_image, input_mask = self.resize(input_image, input_mask)
+		input_image, input_mask = self.normalize(input_image, input_mask)
 
 		return input_image, input_mask
 
-	def get_train_dataset():
-		train_dataset = dataset["train"].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+	def get_train_dataset(self):
+		train_dataset = self.dataset["train"].map(self.load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
 
 		return train_dataset
 
-	def get_test_dataset():
-		test_dataset = dataset["test"].map(load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
+	def get_test_dataset(self):
+		test_dataset = self.dataset["test"].map(self.load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
 
 		return test_dataset
+
+	def get_info(self):
+		return self.info
 
 # train_dataset = dataset["train"].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
 # test_dataset = dataset["test"].map(load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
