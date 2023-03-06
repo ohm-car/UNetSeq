0d97c85a8c9bd9781467a13168a06e53dad6410a
Omkar Kulkarni <90357608+ohm-car@users.noreply.github.com>
2023-03-05 20:39:45 -0500

    Update seqArt.sh

--- seqArt.sh
+++ seqArt.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=seqArt
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --time=10-12:00:00
-#SBATCH --constraint=rtx_2080
+#SBATCH --constraint=rtx_6000
 #SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_1.out
 #SBATCH --error=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_1.err
 
