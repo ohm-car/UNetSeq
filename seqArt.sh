234288ae156886cbd66717153131f9ac1f68ecd4
Omkar Kulkarni <90357608+ohm-car@users.noreply.github.com>
2023-03-05 19:42:09 -0500

    Update seqArt.sh

--- seqArt.sh
+++ seqArt.sh
@@ -3,15 +3,15 @@
 #SBATCH --job-name=seqArt
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --time=10-12:00:00
-#SBATCH --constraint=rtx_8000
+#SBATCH --constraint=rtx_6000
 #SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_1.out
 #SBATCH --error=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_1.err
 
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
-source activate unet
+source activate mrcnn2
 
-python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/k_train.py
+python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/trainFrames.py
