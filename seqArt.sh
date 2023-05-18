1be6be40cb1ca9cd208cf5ba6ae14e1d322dd052
omkar-umbc <omkark1@umbc.edu>
2023-05-18 16:06:38 -0400

    Revisitng this

--- seqArt.sh
+++ seqArt.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=seqArt
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=144G
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --time=10-12:00:00
 #SBATCH --constraint=rtx_6000
 #SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_10.out
@@ -14,4 +14,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
 source activate mrcnn9
 
-python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/trainFrames.py
+python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/train.py
