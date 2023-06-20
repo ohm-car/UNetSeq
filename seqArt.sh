dc3147c9a0a3cdcefb5c332c1643e29fddf516b4
omkar-umbc <omkark1@umbc.edu>
2023-06-19 20:11:00 -0400

    Retry with improved code and h5 models

--- seqArt.sh
+++ seqArt.sh
@@ -7,12 +7,12 @@
 #SBATCH --time=10-12:00:00
 #SBATCH --constraint=rtx_6000
 #SBATCH --exclude=g06
-#SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_10.out
-#SBATCH --error=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_10.err
+#SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_11.out
+#SBATCH --error=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_11.err
 
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
 source activate mrcnn9
 
-python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/train.py
+python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/train.py -b=4 -e=80 -sl=3 -sf=5
