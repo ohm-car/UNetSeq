85b38dd9421f4eedc185d0142675968deadc2acc
Omkar Kulkarni <90357608+ohm-car@users.noreply.github.com>
2023-06-24 15:35:16 -0400

    Update seqArt.sh

--- seqArt.sh
+++ seqArt.sh
@@ -7,12 +7,12 @@
 #SBATCH --time=10-12:00:00
 #SBATCH --constraint=rtx_8000
 #SBATCH --exclude=g06
-#SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_13.out
-#SBATCH --error=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_13.err
+#SBATCH --output=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_14.out
+#SBATCH --error=/nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/outfiles/seqArt_run_14.err
 
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
 source activate mrcnn9
 
-python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/train.py -b=8 -e=80 -sl=3 -sf=5
+python /nfs/ada/oates/users/omkark1/ArteryProj/UNetSeq/train.py -b=4 -e=320 -sl=5 -sf=5
